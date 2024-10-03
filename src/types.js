492c836f091e42675b16a49c77f0107243c2fdcc
meigo <meigo@animal.ee>
2024-10-03 18:38:55 +0300

    update

--- src/types.js
+++
@@ -1,9 +0,0 @@
-/**
- * @typedef {"et"|"en"} Language
- */
-
-/**
- * @typedef {Object} PictureData
- * @property {{type:string, srcset:string}[]} sources
- * @property {{src:string, alt:string, w:number, h:number}} img
- */
